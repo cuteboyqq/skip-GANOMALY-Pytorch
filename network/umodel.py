264564425dcf2d9fac3ef313047f4d927df1fc01
cuteboyqq <58428559+cuteboyqq@users.noreply.github.com>
2023-01-01 18:41:18 +0800

    Add files via upload

--- network/umodel.py
+++ network/umodel.py
@@ -62,12 +62,12 @@ class UGanomaly(nn.Module):
 
         ##
         if self.resume != '':
-            print("\nLoading pre-trained networks.")
+            print("\nLoading pre-trained best networks.")
             #self.iter = torch.load(os.path.join(self.resume, 'netG.pt'))['epoch']
             #self.netg.load_state_dict(torch.load(os.path.join(self.resume, 'netG.pt'))['state_dict'])
             #self.netd.load_state_dict(torch.load(os.path.join(self.resume, 'netD.pt'))['state_dict'])
-            self.netg.load_state_dict(torch.load(os.path.join(self.resume, 'netG.pt')))
-            self.netd.load_state_dict(torch.load(os.path.join(self.resume, 'netD.pt')))
+            self.netg.load_state_dict(torch.load(os.path.join(self.resume, 'best_netG.pt')))
+            self.netd.load_state_dict(torch.load(os.path.join(self.resume, 'best_netD.pt')))
             print("\tDone.\n")
 
         self.l_adv = l2_loss
@@ -291,5 +291,224 @@ class UGanomaly(nn.Module):
             ##
             # RETURN
             return auc
+    ##
+    def plot_loss_histogram(self,loss_list, name):
+        from matplotlib import pyplot
+        import numpy
+        import matplotlib.pyplot as plt
+        bins = numpy.linspace(0,18, 100)
+        pyplot.hist(loss_list, bins=bins, alpha=0.5, label=name)
+        os.makedirs('./runs/detect',exist_ok=True)
+        filename = str(name) + '.jpg'
+        file_path = os.path.join('./runs/detect',filename)
+        plt.savefig(file_path)
+        plt.show()
+    ##
+    #https://stackoverflow.com/questions/6871201/plot-two-histograms-on-single-chart-with-matplotlib
+    def plot_two_loss_histogram(self,normal_list, abnormal_list, name):
+        import numpy
+        from matplotlib import pyplot
+        import matplotlib.pyplot as plt
+        bins = numpy.linspace(0, 18, 100)
+        pyplot.hist(normal_list, bins, alpha=0.5, label='normal')
+        pyplot.hist(abnormal_list, bins, alpha=0.5, label='abnormal')
+        pyplot.legend(loc='upper right')
+        os.makedirs('./runs/detect',exist_ok=True)
+        filename = str(name) + '.jpg'
+        file_path = os.path.join('./runs/detect',filename)
+        plt.savefig(file_path)
+        pyplot.show()
         
-  
+    ## 
+    def Analysis_two_list(self, normal_list, abnormal_list, name, user_loss_list=None):
+        import math
+        import numpy
+        normal_count_list = [0]*len(user_loss_list)
+        abnormal_count_list = [0]*len(user_loss_list)
+        for i in range(len(normal_list)):
+            normal_count_list[int(normal_list[i])]+=1
+        print('normal_count_list')
+        for i in range(len(normal_count_list)):
+            print('{}: {}'.format(i,normal_count_list[i]))
+        
+        for i in range(len(abnormal_list)):
+            abnormal_count_list[int(abnormal_list[i])]+=1
+        print('abnormal_count_list')
+        for i in range(len(abnormal_count_list)):
+            print('{}: {}'.format(i,abnormal_count_list[i]))
+        
+        overlap_normal_count = 0
+        overlap_abnormal_count = 0
+        overlap_count = 0
+        for i in range(len(normal_count_list)):
+            if normal_count_list[i]!=0 and abnormal_count_list[i]!=0:
+                overlap_normal_count += normal_count_list[i]
+                overlap_abnormal_count += abnormal_count_list[i]
+                overlap_count += min(normal_count_list[i],abnormal_count_list[i])
+        print('overlap_normal_count: {}'.format(overlap_normal_count))
+        print('overlap_abnormal_count: {}'.format(overlap_abnormal_count))
+        print('overlap_count: {}'.format(overlap_count))
+        
+        from matplotlib import pyplot
+        bins = numpy.linspace(0, 13, 100)
+        pyplot.hist(normal_list, bins, alpha=0.5, label='normal')
+        pyplot.hist(abnormal_list, bins, alpha=0.5, label='abnormal')
+        pyplot.legend(loc='upper right')
+        os.makedirs('./runs/detect',exist_ok=True)
+        filename = str(name) + '.jpg'
+        file_path = os.path.join('./runs/detect',filename)
+        pyplot.savefig(file_path)
+        pyplot.show()
+        
+        if user_loss_list is None:
+            normal_acc,abnormal_acc = self.Get_lossTH_Accuracy(normal_count_list,abnormal_count_list)
+        else:
+            normal_acc,abnormal_acc = self.Get_lossTH_Accuracy_UserDefineLossTH(normal_count_list,abnormal_count_list, user_loss_list)
+        
+        return normal_count_list,abnormal_count_list,normal_acc,abnormal_acc
+    ##
+    def Analysis_two_list_UserDefineLossTH(self, normal_list, abnormal_list, name, user_loss_list=None):
+        show_log = False
+        import math
+        import numpy
+        normal_count_list = [0]*len(user_loss_list)
+        abnormal_count_list = [0]*len(user_loss_list)
+        
+        user_loss_list = sorted(user_loss_list)
+        
+        if show_log:
+            print('normal_list : {}'.format(normal_list))
+            print('abnormal_list : {}'.format(abnormal_list))
+            print('user_loss_list : {}'.format(user_loss_list))
+        
+        for i in range(len(user_loss_list)):
+            for j in range(len(normal_list)):
+                if (i+1) < len(user_loss_list):
+                    if normal_list[j] >= user_loss_list[i] and  normal_list[j] < user_loss_list[i+1]:
+                        normal_count_list[i]+=1
+                else:
+                    if normal_list[j] >= user_loss_list[i]:
+                        normal_count_list[i]+=1
+        
+        for i in range(len(user_loss_list)):
+            for j in range(len(abnormal_list)):
+                if (i+1) < len(user_loss_list):
+                    if abnormal_list[j] >= user_loss_list[i] and  abnormal_list[j] < user_loss_list[i+1]:
+                        abnormal_count_list[i]+=1
+                else:
+                    if abnormal_list[j] >= user_loss_list[i]:
+                        abnormal_count_list[i]+=1
+                
+        normal_acc,abnormal_acc = self.Get_lossTH_Accuracy_UserDefineLossTH(normal_count_list,abnormal_count_list, user_loss_list)
+        
+        print('user_loss_list: {}'.format(user_loss_list))
+        
+        print('normal_count_list:') 
+        for i in range(len(user_loss_list)):
+            print('{} : {}'.format(user_loss_list[i], normal_count_list[i]))
+            
+        print('abnormal_count_list:')
+        for i in range(len(user_loss_list)):
+            print('{} : {}'.format(user_loss_list[i], abnormal_count_list[i]))
+            
+            
+        #print('normal_count_list: {}'.format(normal_count_list))
+        #print('abnormal_count_list: {}'.format(abnormal_count_list))
+        
+        return normal_count_list,abnormal_count_list,normal_acc,abnormal_acc
+    ##
+    def Analysis_Accuracy(self, normal_count_list,abnormal_count_list,loss_th=3.0):
+        show_log = False
+        normal_correct_cnt = 0
+        total_normal_cnt = 0
+        for i in range(len(normal_count_list)):
+            total_normal_cnt+=normal_count_list[i]
+            if i < loss_th:
+                normal_correct_cnt+=normal_count_list[i]
+        if show_log:
+            print('normal_correct_cnt: {}'.format(normal_correct_cnt))
+            print('total_normal_cnt: {}'.format(total_normal_cnt))
+        if total_normal_cnt == 0:
+            normal_acc = 0.0
+        else:
+            normal_acc = float(normal_correct_cnt/total_normal_cnt)
+        
+        total_abnormal_cnt = 0
+        abnormal_correct_cnt = 0
+        for i in range(len(abnormal_count_list)):
+            total_abnormal_cnt+=abnormal_count_list[i]
+            if i >= loss_th:
+                abnormal_correct_cnt+=abnormal_count_list[i]
+        if show_log:
+            print('abnormal_correct_cnt : {}'.format(abnormal_correct_cnt))
+            print('total_abnormal_cnt: {}'.format(total_abnormal_cnt))
+        if total_abnormal_cnt==0:
+            abnormal_acc = 0
+        else:
+            abnormal_acc = float(abnormal_correct_cnt / total_abnormal_cnt)
+        
+        
+        return normal_acc,abnormal_acc
+    
+    ##
+    def Analysis_Accuracy_UserDefineLossTH(self, normal_count_list,abnormal_count_list,loss_th=3.0, user_loss_list=None):
+        show_log = False
+        normal_correct_cnt = 0
+        total_normal_cnt = 0
+        for i in range(len(normal_count_list)):
+            total_normal_cnt+=normal_count_list[i]
+            if user_loss_list[i] < loss_th:
+                normal_correct_cnt+=normal_count_list[i]
+        if show_log:
+            print('normal_correct_cnt: {}'.format(normal_correct_cnt))
+            print('total_normal_cnt: {}'.format(total_normal_cnt))
+        if total_normal_cnt == 0:
+            normal_acc = 0.0
+        else:
+            normal_acc = float(normal_correct_cnt/total_normal_cnt)
+        
+        total_abnormal_cnt = 0
+        abnormal_correct_cnt = 0
+        for i in range(len(abnormal_count_list)):
+            total_abnormal_cnt+=abnormal_count_list[i]
+            if user_loss_list[i] >= loss_th:
+                abnormal_correct_cnt+=abnormal_count_list[i]
+        if show_log:
+            print('abnormal_correct_cnt : {}'.format(abnormal_correct_cnt))
+            print('total_abnormal_cnt: {}'.format(total_abnormal_cnt))
+        if total_abnormal_cnt==0:
+            abnormal_acc = 0
+        else:
+            abnormal_acc = float(abnormal_correct_cnt / total_abnormal_cnt)
+        
+        
+        return normal_acc,abnormal_acc
+    
+    ##
+    def Get_lossTH_Accuracy(self, normal_count_list,abnormal_count_list):
+        normal_acc_list,abnormal_acc_list=[0.0]*10,[0.0]*10
+        
+        for i in range(len(normal_acc_list)):
+            normal_acc,abnormal_acc = self.Analysis_Accuracy(normal_count_list,abnormal_count_list,i)
+                  
+            normal_acc_list[i] = normal_acc
+            abnormal_acc_list[i] = abnormal_acc
+            
+        for i in range(len(normal_acc_list)):
+            print('loss {} ,normal acc: {} ,abnormal acc{}'.format(i,normal_acc_list[i],abnormal_acc_list[i]))
+            
+        return normal_acc,abnormal_acc
+    ##
+    def Get_lossTH_Accuracy_UserDefineLossTH(self, normal_count_list,abnormal_count_list, user_loss_list):
+        normal_acc_list,abnormal_acc_list=[0.0]*len(user_loss_list),[0.0]*len(user_loss_list)
+        
+        for i in range(len(user_loss_list)):
+            normal_acc,abnormal_acc = self.Analysis_Accuracy_UserDefineLossTH(normal_count_list,abnormal_count_list,user_loss_list[i],user_loss_list)
+                  
+            normal_acc_list[i] = normal_acc
+            abnormal_acc_list[i] = abnormal_acc
+            
+        for i in range(len(user_loss_list)):
+            print('loss {} ,normal acc: {} ,abnormal acc{}'.format(user_loss_list[i],normal_acc_list[i],abnormal_acc_list[i]))
+            
+        return normal_acc,abnormal_acc
