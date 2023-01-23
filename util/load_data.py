9b6be54da052be84f0a2fd3c24fb02424265b069
cuteboyqq <58428559+cuteboyqq@users.noreply.github.com>
2023-01-23 13:40:49 +0800

    Add files via upload

--- util/load_data.py
+++ util/load_data.py
@@ -8,39 +8,85 @@ import torchvision.transforms as transforms
 import torchvision
 import torch
 
-def load_data(args,train=True,test=False):
+from torch.utils.data import DataLoader
+from torchvision.datasets import MNIST, CIFAR10, ImageFolder
+from util.datasets import get_cifar_anomaly_dataset
+from util.datasets import get_mnist_anomaly_dataset
+
+def load_data(args,train=True,test=False,CUSTOM=True):
     size = (args.img_size,args.img_size)
-    if train:
-        img_data = torchvision.datasets.ImageFolder(args.img_dir,
-                                                    transform=transforms.Compose([
-                                                    transforms.Resize(size),
-                                                    #transforms.RandomHorizontalFlip(),
-                                                    #transforms.Scale(64),
-                                                    transforms.CenterCrop(size),                                                 
-                                                    transforms.ToTensor(),
-                                                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)) #GANomaly parameter
-                                                    ])
-                                                    )
-    elif test:
-        img_data = torchvision.datasets.ImageFolder(args.img_testdir,
-                                                    transform=transforms.Compose([
-                                                    transforms.Resize(size),
-                                                    #transforms.RandomHorizontalFlip(),
-                                                    #transforms.Scale(64),
-                                                    transforms.CenterCrop(size),                                                 
-                                                    transforms.ToTensor(),
-                                                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)) #GANomaly parameter
-                                                    ])
-                                                    )
-    if train:
-        shuffle=True
-        batch_size = args.batch_size
+    ## USE CUSTOM Datasets
+    if CUSTOM:
+        if train:
+            ## CUSTOM
+            
+            img_data = torchvision.datasets.ImageFolder(args.img_dir,
+                                                        transform=transforms.Compose([
+                                                        transforms.Resize(size),
+                                                        #transforms.RandomHorizontalFlip(),
+                                                        #transforms.Scale(64),
+                                                        transforms.CenterCrop(size),                                                 
+                                                        transforms.ToTensor(),
+                                                        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)) #GANomaly parameter
+                                                        ])
+                                                        )
+            
+            
+            
+            
+        elif test:
+            img_data = torchvision.datasets.ImageFolder(args.img_testdir,
+                                                        transform=transforms.Compose([
+                                                        transforms.Resize(size),
+                                                        #transforms.RandomHorizontalFlip(),
+                                                        #transforms.Scale(64),
+                                                        transforms.CenterCrop(size),                                                 
+                                                        transforms.ToTensor(),
+                                                        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)) #GANomaly parameter
+                                                        ])
+                                                        )
+        if train:
+            shuffle=True
+            batch_size = args.batch_size
+        else:
+            shuffle=False
+            batch_size = args.test_batchsize
+        data_loader = torch.utils.data.DataLoader(img_data, batch_size=batch_size,shuffle=shuffle,drop_last=True)
+        print('data_loader length : {}'.format(len(data_loader)))
+        
+    ## USE OPEN SOURCE Datasets (CIFAR10, MNIST)
     else:
-        shuffle=False
-        batch_size = args.test_batchsize
-    data_loader = torch.utils.data.DataLoader(img_data, batch_size=batch_size,shuffle=shuffle,drop_last=True)
-    print('data_loader length : {}'.format(len(data_loader)))
-    return data_loader
+        ## CIFAR
+        if args.img_dir in ['cifar10']:
+            transform = transforms.Compose([transforms.Resize(args.img_size),
+                                            transforms.ToTensor(),
+                                            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    
+            train_ds = CIFAR10(root='./data', train=True, download=True, transform=transform)
+            valid_ds = CIFAR10(root='./data', train=False, download=True, transform=transform)
+            train_ds, valid_ds = get_cifar_anomaly_dataset(train_ds, valid_ds, train_ds.class_to_idx[args.abnormal_class])
+    
+        ## MNIST
+        elif args.img_dir in ['mnist']:
+            transform = transforms.Compose([transforms.Resize(args.img_size),
+                                            transforms.ToTensor(),
+                                            transforms.Normalize((0.1307,), (0.3081,))])
+    
+    
+            train_ds = MNIST(root='./data', train=True, download=True, transform=transform)
+            valid_ds = MNIST(root='./data', train=False, download=True, transform=transform)
+            train_ds, valid_ds = get_mnist_anomaly_dataset(train_ds, valid_ds, int(args.abnormal_class))
+            
+        ## DATALOADER
+        train_dl = DataLoader(dataset=train_ds, batch_size=args.batch_size, shuffle=True, drop_last=True)
+        valid_dl = DataLoader(dataset=valid_ds, batch_size=args.batch_size, shuffle=False, drop_last=False)
+            
+    
+    if CUSTOM:
+        return data_loader
+    else:
+        return train_dl, valid_dl
+        
 
 
 def print_parameters(args):
